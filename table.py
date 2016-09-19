0a8167de005c786145fb0be53aab4ae4c64a2010
naveenbajaj123 <naveen.bajaj3@gmail.com>
2016-09-19 21:32:18 +0530

    Add files via upload
    made very basic without loop 
    But i wnna use function calling itself
    but not getting how?

---
+++ table.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+def table_digit(a):
+	print "Table start\n"		
+        print "17*1=",a*1
+	print "17*2=",a*2
+ 	print "17*3=",a*3
+	print "17*4=",a*4
+    	print "17*5=",a*5
+	print "17*6=",a*6
+	print "17*7=",a*7
+	print "17*8=",a*8
+	print "17*9=",a*9
+	print "17*10=",a*10
+table_digit(17)
