e741011abc3b4a73dd6f6817718fde1e1c52106c
naveenbajaj123 <naveen.bajaj3@gmail.com>
2016-10-10 18:13:52 +0530

    Add files via upload

---
+++ nameformatnew.py
@@ -0,0 +1,19 @@
+def require_srname():
+	srnme=raw_input("enter the user last name\n")
+	return srnme
+def require_name():
+	name=raw_input("entre the user firt name\n")
+	return name
+out1=require_name()
+out2=require_srname()
+new=out1+','+out2
+print new
+other1=out1.swapcase()
+other2=out2.swapcase()
+print other1,other2
+
+
+
+
+
+	
