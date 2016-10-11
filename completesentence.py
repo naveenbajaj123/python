42f152fdfcce52a888824977ed1b7d565e635ed3
naveenbajaj123 <naveen.bajaj3@gmail.com>
2016-10-11 22:18:04 +0530

    Add files via upload

--- completesentence.py
+++ completesentence.py
@@ -5,8 +5,8 @@ out=require_sentence()
 print out
 print  out[-1],    "  last word of sentence" #print last word
 print  out[-5:],   "  last 5 charater of sentence\n",#print last 5 character of the sentence
-print  out[2],     "  2nd position charater of sentence\n"  #print 2nd character
-print  out[5],     "  5th position charater of sentence\n"  #5th character
+print  out[1],     "  2nd position charater of sentence\n"  #print 2nd character
+print  out[4],     "  5th position charater of sentence\n"  #5th character
 new=len(out)/2
 mid=out[new]
 lmid=out[new-1]
