4e0ec2db035bf552d8c402e144aeb135edcaeb8d
naveenbajaj123 <naveen.bajaj3@gmail.com>
2016-10-11 20:15:31 +0530

    Add files via upload

--- completesentence.py
+++ completesentence.py
@@ -3,15 +3,15 @@ def require_sentence():
 	return name
 out=require_sentence()
 print out
-print out[-1] #print last word
-print out[-5:]#print last 5 character of the sentence
-print out[2]  #print 2nd character
-print out[5]  #5th character
+print  out[-1],    "  last word of sentence" #print last word
+print  out[-5:],   "  last 5 charater of sentence\n",#print last 5 character of the sentence
+print  out[2],     "  2nd position charater of sentence\n"  #print 2nd character
+print  out[5],     "  5th position charater of sentence\n"  #5th character
 new=len(out)/2
 mid=out[new]
 lmid=out[new-1]
 rmid=out[new+1]
-print lmid+mid+rmid#print adjing chracter
+print lmid+mid+rmid,    "   midlle char with adjoining charater of the sentence"    #print adjing chracter
 
 
 
